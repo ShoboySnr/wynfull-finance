a6cc4519faf40f3307f0de6bbf98062a6c848c11
Shobowale Damilare <oluwadamilareshobowale@gmail.com>
2025-11-12 20:35:19 +0100

    update

--- resources/views/client/coaching/index.blade.php
+++ resources/views/client/coaching/index.blade.php
@@ -20,8 +20,8 @@
                             @if($coach && $coach->coachProfile && $coach->profile)
                                 <img src="{{ $coach->profile->avatar_url }}" alt="Coach" class="coach-avatar">
                                 <div class="coach-details">
-                                    <h3>{{ $coach->name }},</h3>
-                                    <p>{{ $coach->profile->bio }}</p>
+                                    <h3>{{ $coach->name }}</h3>
+                                    <p>{{ $coach->profile->bio ? ', '. $coach->profile->bio : '' }}</p>
                                 </div>
                             @else
                                 <p>You are not currently assigned to a coach.</p>
